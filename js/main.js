0876fead74ca5ce4731bbb34e9e2d6e117520101
MinhHieu20 <72988848+MinhHieu20@users.noreply.github.com>
2022-08-09 10:11:08 +0700

    hieuUD

--- js/main.js
+++ js/main.js
@@ -49,25 +49,6 @@ cart.addEventListener('click', () => {
     notify.classList.toggle('show')
 })
 
-// ---------------------------------------------------------------------------------
-const modal = document.querySelector(".form--contact");
-const openModalBtn = document.querySelector(".open-modal-btn");
-// const iconCloseModal = document.querySelector(".modal__header i");
-// const buttonCloseModal = document.querySelector(".modal__footer button");
-
-function toggleModal() {
-  modal.classList.toggle("hide");
-}
-
-// openModalBtn.addEventListener("click", toggleModal);
-// iconCloseModal.addEventListener("click", toggleModal);
-// buttonCloseModal.addEventListener("click", toggleModal);
-
-// modal.addEventListener("click", (e) => {
-//   if (e.target == e.currentTarget) toggleModal();
-// });
-
-
 const tabRecruit =$$('.recruit-pagination-list__item')
 const paneRecruit =$$('.main-recruit-content__list')
 
